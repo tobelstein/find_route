505d828f2517691f4432e45924b10014f4e9b548
Maxim Zinyakov <maqs@yandex.ru>
2022-08-01 14:46:58 +0300

    New features added

--- cities/views.py
+++ cities/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from cities.models import City
+from django.views.generic.detail import DetailView
+from .forms import HtmlForm, CityForm
+
 
 __all__ = (
-    "home",
+    "home", "CityDetailView"
 )
 
 # Create your views here.
 def home(request, pk=None):
-    if pk:
-        #city = City.objects.filter(pk=pk).first()
-        city = get_object_or_404(City, pk=pk)
-        context = {'object':city}
-        return render(request, 'cities/detail.html', context)
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+#    if pk:
+#        #city = City.objects.filter(pk=pk).first()
+#        city = get_object_or_404(City, pk=pk)
+#        context = {'object':city}
+#        return render(request, 'cities/detail.html', context)
 
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list':qs}
+    context = {'objects_list':qs, "form":form}
     return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
--- travel/urls.py
+++ travel/urls.py
@@ -20,7 +20,7 @@ from travel.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('', home, name='home'),
     path('about/', about),    
     path('cities/', include(('cities.urls', 'cities'))),
 ]
